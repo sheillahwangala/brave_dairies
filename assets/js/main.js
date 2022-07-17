1a26e6728a4b2ffb494753d10885b8af893153e3
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-07-17 23:41:17 +0300

    add js elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,37 @@
 var productions = new Array();
 
-var totalProduce = 0;
+let totalProduce = 0;
 let price = 45;
 let newPrice = 49;
 
 function totalProduction() {
     var sheds = document.getElementById("sheds");
     var shedNumber = sheds.options[sheds.selectedIndex].text;
-    var dailyProduction = txtDailyProd.value;
+
+    if(txtDailyProd.value===""){
+        window.alert("Please enter daily production of the selected shed.");
+        return;
+    }
+
+    var dailyProduction = parseInt(txtDailyProd.value);
     // console.log(dailyProduction);
     // console.log(shedNumber);
     var prod = { shed: shedNumber, production: dailyProduction };
     productions.push(prod);
-    totalProduce = totalProduce + dailyProduction;
+    totalProduce += dailyProduction;
+    console.log("Total Produce " + totalProduce);
     console.log(productions);
     window.alert("Daily production for " + shedNumber + " added.");
+    txtDailyProd.value = "";
+
+    var curDiv = document.getElementById("totals");
+
+    var newDiv = document.createElement("div");
+    var divContent = document.createTextNode("Your production in " + shedNumber + " will be Ksh " + dailyProduction);
+
+    newDiv.appendChild(divContent);
+
+    curDiv.appendChild(newDiv);
 }
 
 
@@ -36,10 +53,23 @@ function incomeOverTime(selling_price, time) {
     var totalIncome = 0;
     var totalYearIncome = 0;
 
-    console.log("Total Produc "+totalProduce);
+    console.log("Total Produc " + totalProduce);
     console.log(selling_price);
+    console.log(time);
 
     var curDiv = document.getElementById("results");
+    curDiv.innerHTML = "";
+
+    if(selling_price===""){
+        window.alert("Please enter amount per litre to continue.");
+        return;
+    }
+
+    if(time==""){
+        window.alert("Please period to continue.");
+        return;
+    }
+
 
     if (time === "weekly") {
         totalIncome = totalProduce * 7 * selling_price;
@@ -62,5 +92,20 @@ function incomeOverTime(selling_price, time) {
 
         curDiv.appendChild(newDiv);
     }
+}
 
+function wholeYearReport() {
+
+    var curDiv = document.getElementById("results");
+    curDiv.innerHTML = "";
+
+    for (var x = 0; x < months.length; x++) {
+        var newDiv = document.createElement("div");
+        totalIncome = price * totalProduce * months[x].days;
+        var divContent = document.createTextNode("Your income for " + months[x].name + " will be Ksh " + totalIncome);
+        newDiv.appendChild(divContent);
+
+        curDiv.appendChild(newDiv);
+
+    }
 }
